1614ea26258f964a4f141855c929da746c668848
osmanekrem <korkmazosmanekrem@gmail.com>
2024-04-22 21:54:42 +0300

    fix optional github url for admin

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -9,7 +9,7 @@ export const ProjectSchema = z.object({
   title: z.string().min(2),
   description: z.string().min(2),
   liveUrl: z.string().min(2),
-  githubUrl: z.string().min(2),
+  githubUrl: z.string(),
   image: z.string().min(2),
   tags: z.array(z.string())
 })
