58786cb81316a0f29fd619918014571ee625c65a
osmanekrem <korkmazosmanekrem@gmail.com>
2024-05-06 18:53:50 +0300

    bug fix

--- src/app/(protected)/admin/page.tsx
+++ src/app/(protected)/admin/page.tsx
@@ -1,4 +1,4 @@
-import { auth, signOut } from '@/auth'
+import { auth } from '@/auth'
 import React from 'react'
 
 export default async function DashboardPage() {
@@ -6,8 +6,10 @@ export default async function DashboardPage() {
   const session = await auth()
 
   return (
-    <div>DashboardPage
-      {JSON.stringify(session)}
+    <div className="flex w-full flex-col space-y-8 py-8">
+      <div className="flex justify-start items-center w-full ">
+        <h1 className="text-3xl font-semibold">Welcome back {session?.user?.name}!</h1></div>
+      <pre>{JSON.stringify(session,null,2)}</pre>
     </div>
   )
 }
